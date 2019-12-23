62970c8699c436f0c69a5688bb15456d819c8c85
zmjie <1399824158@qq.com>
2019-12-23 14:30:06 +0800

    添加ZMJNoDataView

---
+++ ZMJNoDataView/Classes/ZMJNoDataView.h
@@ -0,0 +1,19 @@
+//
+//  ZMJNoDataView.h
+//  ZMJNoDataView_Example
+//
+//  Created by qx on 2019/12/21.
+//  Copyright © 2019 zmjie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZMJNoDataView : NSObject
+
+- (void)ceshi;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZMJNoDataView/Classes/ZMJNoDataView.m
@@ -0,0 +1,18 @@
+//
+//  ZMJNoDataView.m
+//  ZMJNoDataView_Example
+//
+//  Created by qx on 2019/12/21.
+//  Copyright © 2019 zmjie. All rights reserved.
+//
+
+#import "ZMJNoDataView.h"
+
+@implementation ZMJNoDataView
+
+- (void)ceshi {
+    
+    NSLog(@"12121212121212121212");
+}
+
+@end
